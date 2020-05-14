8a397b82c340446f71db3e3e1dc602729d5f1c7e
Phil Veloso <philveloso@Penguin.home>
2020-05-14 20:05:20 +0200

    misc

--- scrapper.py
+++ scrapper.py
@@ -31,3 +31,38 @@ sign_in_button = driver.find_element_by_xpath('//*[@type="submit"]')
 
 # .click() to mimic button click
 sign_in_button.click()
+
+# locate search form by_name
+search_query = driver.find_element_by_name('q')
+
+# send_keys() to simulate the search text key strokes
+search_query.send_keys('site:linkedin.com/in/ AND "python developer" AND "London"')
+
+# .send_keys() to simulate the return key 
+search_query.send_keys(Keys.RETURN)
+
+# locate URL by_class_name
+linkedin_urls = driver.find_elements_by_class_name('iUh30')
+
+# variable linkedin_url is equal to the list comprehension 
+linkedin_urls = [url.text for url in linkedin_urls]
+
+# to print all elements within our list 
+linkedin_urls
+
+driver.get('https:www.google.com')
+sleep(3)
+
+search_query = driver.find_element_by_name('q')
+search_query.send_keys(parameters.search_query)
+sleep(0.5)
+
+search_query.send_keys(Keys.RETURN)
+sleep(3)
+
+linkedin_urls = driver.find_elements_by_class_name('iUh30')
+linkedin_urls = [url.text for url in linkedin_urls]
+sleep(0.5)
+
+driver.quit()
+
